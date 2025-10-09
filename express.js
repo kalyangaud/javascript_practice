16276af4beb5d56fa1b2132269062dbbfe5b0d11
kalyangaud <081bei015@ioepc.edu.np>
2025-10-09 09:51:43 +0545

    world express

---
+++ express.js
@@ -0,0 +1,18 @@
+const express = require("express");
+function calculateSum(n) {
+  let ans = 0;
+  for (let i = 1; i < n; i++) {
+    ans = ans + i;
+  }
+  return ans;
+}
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+  const n = req.query.n;
+  const ans = calculateSum(n);
+  res.send(ans.toString());
+});
+
+app.listen(port);
