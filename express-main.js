b26633e575d68c0615f4303d5a0bd1270488e22c
kalyangaud <081bei015@ioepc.edu.np>
2025-10-09 10:25:53 +0545

    server2

--- express-main.js
+++ express-main.js
@@ -8,8 +8,8 @@ function sum(a,b)
 }
 
 app.get("/",function(req,res){
-    const a = parseFloat(req.query.a);
-    const b = parseFloat(req.query.b);
+    const a = (req.query.a);
+    const b = (req.query.b);
     const ans = sum(a,b);
     res.send("hi there! Your answer is "+ ans);
 })
